fd2882e540e2bf1cdb855f3c9cc209420a88c370
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-11-29 21:55:36 +0545

    homepage generating only ui

---
+++ lib/feature_box.dart
@@ -0,0 +1,37 @@
+import 'package:chatgpt_bot/pallete.dart';
+import 'package:flutter/material.dart';
+
+class FeatureBox extends StatelessWidget {
+  final Color color;
+  final String headerText;
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.headerText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(headerText,
+           style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chatgpt_bot/home_page.dart';
+import 'package:chatgpt_bot/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My assistant',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
